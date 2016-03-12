9384eb5c7242b711332c64eb9070ffef29c7b058
op8 <jrc300@gmail.com>
2016-03-12 15:18:52 -0800

    made column a variable

--- qtools-clojure/src/qtools_clojure/algos/buy_signal1.clj
+++ qtools-clojure/src/qtools_clojure/algos/buy_signal1.clj
@@ -2,11 +2,11 @@
 (use 'clojure.java.io)
 (require '[clojure.string :as str])
 
-(defn get-list [lines]
+(defn get-list [lines col]
 	  (loop [x 0 list-vals []]
 			(if (< x 10)
 	  			(recur (inc x) (into list-vals (set [(nth (str/split (nth lines x) #",") 1)] )))
-	  			(take-last (- (count list-vals) 1) list-vals)))
+	  			(take-last (- (count list-vals) col) list-vals)))
 	  
 	)
 
@@ -19,7 +19,7 @@
 
 	(with-open [rdr (reader "/home/lrrr/Quant/data/WIKI-AAPL.csv")]
 		(def lines (line-seq rdr))
-		(def open-vals (get-list lines))
+		(def open-vals (get-list lines 1))
 
 	  )
 	
